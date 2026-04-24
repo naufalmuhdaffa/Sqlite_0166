0ab0c7edc242f4c1e3522af979345c44c6a210e2
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-04-24 10:30:39 +0700

    add abstract class for bloc user event

---
+++ lib/bloc/user_event.dart
@@ -0,0 +1,16 @@
+import '../../domain/entities/user_entity.dart';
+
+abstract class UserEvent {}
+class LoadUsers extends UserEvent {}
+class AddUserEvent extends UserEvent {
+  final UserEntity user;
+  AddUserEvent(this.user);
+}
+class DeleteUserEvent extends UserEvent {
+  final String id;
+  DeleteUserEvent(this.id);
+}
+class UpdateUserEvent extends UserEvent {
+  final UserEntity user;
+  UpdateUserEvent(this.user);
+}
