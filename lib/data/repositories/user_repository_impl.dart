38f359f7cdf202fcacfb872e95a3eddadcc43975
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-04-24 10:29:50 +0700

    add implementation for user repository

---
+++ lib/data/repositories/user_repository_impl.dart
@@ -0,0 +1,49 @@
+import '../../domain/entities/user_entity.dart';
+import '../../domain/repository/user_repository.dart';
+import '../models/user_model.dart';
+import '../../helper/database_helper.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final DatabaseHelper dbHelper;
+  UserRepositoryImpl(this.dbHelper);
+
+  @override
+  Future<List<UserEntity>> getAllUsers() async {
+    final db = await dbHelper.database;
+    final List<Map<String, dynamic>> maps = await db.query('users');
+    return maps.map((userMap) => UserModel.fromMap(userMap)).toList();
+  }
+
+  @override
+  Future<void> addUser(UserEntity user) async {
+    final db = await dbHelper.database;
+    final userModel = UserModel(
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    );
+    await db.insert('users', userModel.toMap());
+  }
+
+  @override
+  Future<void> updateUser(UserEntity user) async {
+    final db = await dbHelper.database;
+    final userModel = UserModel(
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    );
+    await db.update(
+      'users',
+      userModel.toMap(),
+      where: 'id = ?',
+      whereArgs: [user.id],
+    );
+  }
+
+  @override
+  Future<void> deleteUser(String id) async {
+    final db = await dbHelper.database;
+    await db.delete('users', where: 'id = ?', whereArgs: [id]);
+  }
+}
