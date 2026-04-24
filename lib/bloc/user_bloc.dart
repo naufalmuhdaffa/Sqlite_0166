445887ca58645f231a2e90a8c39d7942fb2deea5
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-04-24 10:31:25 +0700

    add user bloc for managing user events and states

---
+++ lib/bloc/user_bloc.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'user_event.dart';
+import 'user_state.dart';
+import '../../domain/repository/user_repository.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  final UserRepository repository;
+
+  UserBloc(this.repository) : super(UserInitial()) {
+    on<LoadUsers>((event, emit) async {
+      emit(UserLoading());
+      try {
+        final users = await repository.getAllUsers();
+        emit(UserLoaded(users));
+      } catch (e) {
+        emit(UserError("Gagal memuat data"));
+      }
+    });
+
+    on<AddUserEvent>((event, emit) async {
+      await repository.addUser(event.user);
+      add(LoadUsers());
+    });
+
+    on<UpdateUserEvent>((event, emit) async {
+      await repository.updateUser(event.user);
+      add(LoadUsers());
+    });
+
+    on<DeleteUserEvent>((event, emit) async {
+      await repository.deleteUser(event.id);
+      add(LoadUsers());
+    });
+  }
+}
